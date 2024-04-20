6c1ed1bd1a31c5dfc4b9021cf56b317dd4c1fd6a
julozowo <j.lozowoj@micromata.de>
2024-04-20 23:55:49 +0200

    added spring starter web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 //TODO: add specific versioning
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql")
