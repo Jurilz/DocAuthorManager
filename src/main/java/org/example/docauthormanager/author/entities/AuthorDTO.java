66fa8c9d4870d339e5c218da58d297b2a2581ed8
julozowo <j.lozowoj@micromata.de>
2024-04-22 00:32:42 +0200

    added author dto

---
+++ src/main/java/org/example/docauthormanager/author/entities/AuthorDTO.java
@@ -0,0 +1,8 @@
+package org.example.docauthormanager.author.entities;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthorDTO(
+        @NotBlank(message = "Authors must have a first name") String firstName,
+        @NotBlank(message = "Authors must have a last name") String latName) {
+}
