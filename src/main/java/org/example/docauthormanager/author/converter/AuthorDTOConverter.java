19fd874042ba39d94ea3b247ca01e88d4414d069
julozowo <j.lozowoj@micromata.de>
2024-04-22 23:55:35 +0200

    added non null annotation

--- src/main/java/org/example/docauthormanager/author/converter/AuthorDTOConverter.java
+++ src/main/java/org/example/docauthormanager/author/converter/AuthorDTOConverter.java
@@ -3,11 +3,13 @@ package org.example.docauthormanager.author.converter;
 import org.example.docauthormanager.author.entities.Author;
 import org.example.docauthormanager.author.entities.AuthorDTO;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorDTOConverter implements Converter<AuthorDTO, Author> {
     @Override
+    @NonNull
     public Author convert(AuthorDTO source) {
         return new Author(
             source.firstName(),
