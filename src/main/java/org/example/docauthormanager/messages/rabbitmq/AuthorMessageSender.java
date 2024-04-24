cc67d26817348a5ca6e0f8ed9dc66178dc657906
julozowo <j.lozowoj@micromata.de>
2024-04-25 00:11:56 +0200

    added event and serialization

--- src/main/java/org/example/docauthormanager/messages/rabbitmq/AuthorMessageSender.java
+++ src/main/java/org/example/docauthormanager/messages/rabbitmq/AuthorMessageSender.java
@@ -1,18 +1,29 @@
 package org.example.docauthormanager.messages.rabbitmq;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.docauthormanager.author.events.AuthorEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorMessageSender {
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
 
-    public AuthorMessageSender(RabbitTemplate rabbitTemplate) {
+    public AuthorMessageSender(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
         this.rabbitTemplate = rabbitTemplate;
+        this.objectMapper = objectMapper;
     }
 
-    public void sendAuthorMessage(String message) {
+    public void sendAuthorMessage(AuthorEvent eventMessage) {
+        String stringMessage;
+        try {
+            stringMessage = objectMapper.writeValueAsString(eventMessage);
+        } catch (JsonProcessingException e) {
+            stringMessage = eventMessage.toString();
+        }
         rabbitTemplate.convertAndSend(RabbitMqConfiguration.EXCHANGE_NAME, RabbitMqConfiguration.AUTHOR_KEY,
-                message);
+                stringMessage);
     }
 }
