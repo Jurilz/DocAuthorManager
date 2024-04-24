cff80164a8ad1b0723548aa14ed62a587ff47caf
julozowo <j.lozowoj@micromata.de>
2024-04-25 00:09:45 +0200

    fixed typo

--- src/main/java/org/example/docauthormanager/author/entities/AuthorDTO.java
+++ src/main/java/org/example/docauthormanager/author/entities/AuthorDTO.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 
 public record AuthorDTO(
         @NotBlank(message = "Authors must have a first name") String firstName,
-        @NotBlank(message = "Authors must have a last name") String latName) {
+        @NotBlank(message = "Authors must have a last name") String lastName) {
 }
