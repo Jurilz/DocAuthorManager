b844d6bfdbae0dc8c5a5f6810ce8d092fb8b62c1
julozowo <j.lozowoj@micromata.de>
2024-04-24 23:38:33 +0200

    added document repository method find by title and authors

--- src/main/java/org/example/docauthormanager/document/repository/DocumentRepository.java
+++ src/main/java/org/example/docauthormanager/document/repository/DocumentRepository.java
@@ -1,7 +1,13 @@
 package org.example.docauthormanager.document.repository;
 
+import org.example.docauthormanager.author.entities.Author;
 import org.example.docauthormanager.document.entities.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface DocumentRepository extends JpaRepository<Document, Long> {
+
+    Optional<Document> findDocumentByTitleAndAuthors(String title, Set<Author> authors);
 }
